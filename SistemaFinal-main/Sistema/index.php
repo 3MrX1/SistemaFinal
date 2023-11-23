f005086c02281db10c0f305f7a04073a97ef7cf0
3MrX1 <126796183+3MrX1@users.noreply.github.com>
2023-11-23 12:19:51 -0300

    Add files via upload
    MODIFICAÇÕES DO DIA 23/11/2023

---
+++ SistemaFinal-main/Sistema/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VER SE TENHO DIREITO AO BOLSA FAMILIA</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body class="bg-black">
+    <div class="container text-center mt-2 ">
+        <img src="Recursos/logo.gif" alt="200px">
+
+
+        <div>
+            <h2><font color="#ffffff">VER SE TENHO DIREITO AO BOLSA FAMILIA</font></h2>
+
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+            <div class="conteiner mt-2"> <a class="btn btn-dark" href="Web/cadastrar.php">Cadastrar</a>
+            </div>
+        </div>
+
+        <div class="conteiner mt-2">
+            <a class="btn btn-dark" href="Web/principal.php">Login</a>
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+        </script>
+
+
+
+</body>
+
+</html>
